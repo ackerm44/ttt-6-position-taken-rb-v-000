999b0a5967e7249db567c3074900c5ee03e8378c
Amanda Ackerman <ackerm44@gmail.com>
2017-10-16 17:22:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(array, index_number)
-  if array[index_number] != "X" || array[index_number] != "O"
+  if array[index_number] = "X" || array[index_number] = "O"
     return true
   else
     return false
