3ed7087cdd89406beb84c8b877247f48e131a668
Amanda Ackerman <ackerm44@gmail.com>
2017-10-16 17:25:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(array, index_number)
   if array[index_number] == "X" || array[index_number] == "O"
     return true
-  #elsif array[index_number] == " " || array[index_number] == "" || array[index_number] == nil
-    #return false
+  else
+    return false
   end
 end
